018043b2590a10f329f5c630e136326a5d66d619
igor <igor@DESKTOP-H1HTBV1>
2024-04-12 08:57:31 -0300

    Implement exception handling middleware to return standardized JSON responses in case of internal server errors (code 500).

---
+++ src/Manager.API/Middlewares/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using Manager.API.Utils;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+
+namespace Manager.API.Middlewares
+{
+    public static class ExceptionHandlerMiddleware
+    {
+        public static void UseCustomExceptionHandler(this IApplicationBuilder app)
+        {
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>
+                {
+                    context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                    context.Response.ContentType = "application/json";
+
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+
+                    if (contextFeature != null)
+                        await context.Response.WriteAsJsonAsync(Responses.InternalServerErrorMessage());
+                });
+            });
+        }
+    }
+}
