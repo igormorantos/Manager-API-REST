a3138e56c9f8dc2536b49517c5afdf5fd0cf1f35
igor <igor@DESKTOP-H1HTBV1>
2024-04-12 11:56:28 -0300

    updating view models

--- src/Manager.API/ViewModels/ResultViewModel.cs
+++ src/Manager.API/ViewModels/ResultViewModel.cs
@@ -6,9 +6,7 @@ namespace Manager.API.ViewModels
     public class ResultViewModel
     {
         public string Message { get; set; }
-
         public bool Success { get; set; }
-
         public dynamic Data { get; set; }
     }
 }
