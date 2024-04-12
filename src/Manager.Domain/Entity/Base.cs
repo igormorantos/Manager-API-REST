7221d73cb51c49584de134d029a42c6a16b0a295
igor <igor@DESKTOP-H1HTBV1>
2024-04-12 09:01:50 -0300

    Building a Base Entity

--- src/Manager.Domain/Entity/Base.cs
+++ src/Manager.Domain/Entity/Base.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FluentValidation.Results;
+using FluentValidation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,9 +13,39 @@ namespace Manager.Domain.Entity
         public Guid Id { get; set; }
 
         internal List<string> _errors;
-
         public IReadOnlyCollection<string> Errors => _errors;
 
-        public abstract bool Validate();
+        public bool IsValid
+            => _errors.Count == 0;
+
+        private void AddErrorList(IList<ValidationFailure> errors)
+        {
+            foreach (var error in errors)
+                _errors.Add(error.ErrorMessage);
+        }
+
+        private void CleanErrors()
+            => _errors.Clear();
+
+        protected bool Validate<T, J>(T validator, J obj)
+            where T : AbstractValidator<J>
+        {
+            var validation = validator.Validate(obj);
+
+            if (validation.Errors.Count > 0)
+                AddErrorList(validation.Errors);
+
+            return _errors.Count == 0;
+        }
+
+        public string ErrorsToString()
+        {
+            var builder = new StringBuilder();
+
+            foreach (var error in _errors)
+                builder.Append(" " + error);
+
+            return builder.ToString();
+        }
     }
 }
