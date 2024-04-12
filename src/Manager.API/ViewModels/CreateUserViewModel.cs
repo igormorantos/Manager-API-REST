65b18eb8568b9f8f1f7af903b3a5857feba58133
igor <igor@DESKTOP-H1HTBV1>
2024-04-12 11:54:22 -0300

    updating view models

--- src/Manager.API/ViewModels/CreateUserViewModel.cs
+++ src/Manager.API/ViewModels/CreateUserViewModel.cs
@@ -4,15 +4,17 @@ namespace Manager.API.ViewModels
 {
     public class CreateUserViewModel
     {
-        [Required(ErrorMessage = "o nome não pode ser vazio")]
-        [MinLength(3, ErrorMessage = "O Nome deve ter no minimo 3 caracteres")]
-        [MaxLength(80, ErrorMessage = "O Nome deve ter no minimo 80 caracteres")]
+
+        [Required(ErrorMessage = "O nome não pode ser vazio.")]
+        [MinLength(3, ErrorMessage = "O nome deve ter no mínimo 3 caracteres.")]
+        [MaxLength(80, ErrorMessage = "O nome deve ter no máximo 80 caracteres.")]
         public string Name { get; set; }
 
-        [Required(ErrorMessage = "o email não pode ser vazio")]
-        [MinLength(10, ErrorMessage = "O email deve ter no minimo 10 caracteres")]
-        [MaxLength(180, ErrorMessage = "O Nome deve ter no minimo 80 caracteres")]
-        [RegularExpression(@"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$",ErrorMessage = "O email informado não é válido.")]
+        [Required(ErrorMessage = "O email não pode ser vazio.")]
+        [MinLength(10, ErrorMessage = "O email deve ter no mínimo 10 caracteres.")]
+        [MaxLength(180, ErrorMessage = "O email deve ter no máximo 180 caracteres.")]
+        [RegularExpression(@"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$",
+                        ErrorMessage = "O email informado não é válido.")]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "A senha não pode ser vazia.")]
