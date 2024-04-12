287b1cf05cb5e07cc6b5de72498157174746f44a
igor <igor@DESKTOP-H1HTBV1>
2024-04-12 08:53:40 -0300

    convert data without having to write manual code for each conversion.

---
+++ src/Manager.API/Configuration/MappingConfig.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Manager.API.ViewModels;
+using Manager.API.ViewModes;
+using Manager.Domain.Entity;
+using Manager.Services.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager.API.Configuration
+{
+ 
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config => {
+                config.CreateMap<User, UserDTO>().ReverseMap();
+                config.CreateMap<CreateUserViewModel, UserDTO>().ReverseMap();
+                config.CreateMap<UpdateUserViewModel, UserDTO>().ReverseMap();
+            });
+
+            return mappingConfig;
+        }
+    }
+}
